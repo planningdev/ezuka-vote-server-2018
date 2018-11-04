23794b46fcb8a5f07a2d29f7c4031194c349b246
Tomoki Ichikawa <ichikawa1060@gmail.com>
2018-11-04 18:12:52 +0900

    Fix: typo

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,6 +1,6 @@
 module Api
   module V1
-    class ApplicationController < ApplicationController
+    class ApplicationController < ::ApplicationController
       protect_from_forgery with: :null_session
     end
   end
