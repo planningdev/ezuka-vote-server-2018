a9960cf2b7b5ebfa6f808d18eda13411b826c241
Tomoki Ichikawa <ichikawa1060@gmail.com>
2018-11-04 12:57:33 +0900

    CloudinaryにアップロードできるようにUplodaerを修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,8 +1,23 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  storage :file
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+
+  if Rails.env.production?
+    include Cloudinary::CarrierWave
+
+    process :convert => 'png'
+    process :tags => ['ezuka2018']
+
+    process :resize_to_limit => [1000, 1000]
+
+    def public_id
+      return "ezuka2018/" + Cloudinary::Utils.random_public_id;
+    end
+  else
+    storage :file
+
+    def store_dir
+      "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    end
   end
 end
