72613876bfddee7ed05873187a4dee4403a33740
Tomoki Ichikawa <ichikawa1060@gmail.com>
2018-11-04 18:29:09 +0900

    Add: GET `/api/v1/apps/:id`  `/api/v1/ideas/:id`

--- app/controllers/api/v1/apps_controller.rb
+++ app/controllers/api/v1/apps_controller.rb
@@ -4,14 +4,22 @@ module Api
       include TokenAuthorizationConcern
 
       before_action :set_apps, only: %i(index)
+      before_action :set_app, only: %i(show)
 
       def index
+      end
+
+      def show
       end
 
       private
 
       def set_apps
         @apps = App.all
+      end
+
+      def set_app
+        @app = App.find(params[:id])
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :vote_tokens, only: %i(show), param: :token
-      resources :apps, only: %i(index) do
+      resources :apps, only: %i(index show) do
         resource :vote, only: %i(create), module: :apps
       end
-      resources :ideas, only: %i(index) do
+      resources :ideas, only: %i(index show) do
         resource :vote, only: %i(create), module: :ideas
       end
     end
