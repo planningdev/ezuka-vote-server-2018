1da0eb5d3f153f6b12c2b43e5caf19a08380ae1f
Tomoki Ichikawa <ichikawa1060@gmail.com>
2018-11-06 00:14:04 +0900

    apps#edit作成

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,12 +1,23 @@
 class AppsController < ApplicationController
   before_action :basic_authentication!, only: %i(index)
   before_action :set_apps, only: %i(index)
-  before_action :set_app, only: %i(show)
+  before_action :set_app, only: %i(show edit update )
 
   def index
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @app.update(app_params)
+      redirect_to @app
+    else
+      render 'edit', alert: 'エラーがあります。'
+    end
   end
 
   private
@@ -17,5 +28,9 @@ class AppsController < ApplicationController
 
   def set_app
     @app = App.find_by_encrypted_id(params[:encrypted_id])
+  end
+
+  def app_params
+    params.require(:app).permit(:title, :description, images_attributes: %i(id image image_cache _destroy))
   end
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,6 +1,9 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
+  def extension_white_list
+    %w(jpg jpeg png)
+  end
 
   if Rails.env.production?
     include Cloudinary::CarrierWave
@@ -19,5 +22,8 @@ class ImageUploader < CarrierWave::Uploader::Base
     def store_dir
       "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
     end
+  end
+  def cache_dir
+    'cache'
   end
 end
